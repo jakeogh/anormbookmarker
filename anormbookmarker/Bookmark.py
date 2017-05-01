b1323cada6eea374723e2caab4b04ce13557397b
jakeogh <github.com@v6y.net>
2017-04-30 17:40:10 -0700

    fix imports

--- anormbookmarker/Bookmark.py
+++ anormbookmarker/Bookmark.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # MIT License
 
-from BookmarkClassConstructor import BookmarkClassConstructor
-from Filename import Filename
+from .BookmarkClassConstructor import BookmarkClassConstructor
+from .Filename import Filename
 Bookmark = BookmarkClassConstructor(mapper_to_bookmark=Filename)
 
