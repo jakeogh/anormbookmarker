162bdebee614453b0870013d973f4f40afcb8668
jakeogh <github.com@v6y.net>
2017-04-30 17:30:13 -0700

    auto-commit

--- anormbookmarker/Tag.py
+++ anormbookmarker/Tag.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # MIT License
 
-from TagClassConstructor import TagClassConstructor
-from Filename import Filename
+
+
+from .TagClassConstructor import TagClassConstructor
+from .Filename import Filename
 Tag = TagClassConstructor(mapper_to_bookmark=Filename)
