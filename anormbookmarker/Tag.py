9e88a214972cdd54ccc2f3e5019c7fa565258c96
jakeogh <github.com@v6y.net>
2017-04-30 17:48:21 -0700

    move Tag class construction

--- anormbookmarker/Tag.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# MIT License
-
-
-
-from .TagClassConstructor import TagClassConstructor
-from .Filename import Filename
-Tag = TagClassConstructor(mapper_to_bookmark=Filename)
