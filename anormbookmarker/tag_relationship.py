1e3aa540d973e86e726814b0aacccd50c6e4f84f
jakeogh <github.com@v6y.net>
2017-05-05 21:36:41 -0700

    auto-commit

--- anormbookmarker/tag_relationship.py
+++ anormbookmarker/tag_relationship.py
@@ -7,12 +7,15 @@ from sqlalchemy import Column
 from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
 from sqlalchemy import UniqueConstraint
+from sqlalchemy import CheckConstraint
 from .BaseMixin import BASE
+
+tag_constraint = 'tag_id!=tag_parent_id''
 
 tag_relationship = Table(
     'tag_relationship', BASE.metadata,
     Column('tag_parent_id', Integer, ForeignKey('tag.id')),
     Column('tag_id', Integer, ForeignKey('tag.id')),
-    UniqueConstraint('tag_id', 'tag_parent_id'))
+    CheckConstraint(tag_constraint))
 
 
