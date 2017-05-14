9e16706eedc445d9ffa88d39530061701c48a0a1
jakeogh <github.com@v6y.net>
2017-05-14 15:20:06 -0700

    auto-commit

--- anormbookmarker/BaseMixin.py
+++ anormbookmarker/BaseMixin.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # MIT License
-'''
-Base class to automatically name tables.
-'''
+
+'''Base class to automatically name tables.'''
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.declarative import declared_attr
@@ -12,7 +11,7 @@ class BaseMixin(object):
     '''Base Mixin class to automate table naming.'''
     @declared_attr
     def __tablename__(cls):
-        return cls.__name__.lower()
+        return cls.__name__.lower().split('.')[-1]
     @classmethod
     def query(cls, session):
         return session.query_property()
