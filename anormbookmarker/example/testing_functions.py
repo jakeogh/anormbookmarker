f8df63d81562920eaef89a2d17095d6e7b8d43b8
jakeogh <github.com@v6y.net>
2017-05-14 14:39:29 -0700

    remove old function

--- anormbookmarker/example/testing_functions.py
+++ anormbookmarker/example/testing_functions.py
@@ -28,64 +28,6 @@ logging.basicConfig()
 logging.getLogger('sqlalchemy.engine').setLevel(logging.CRITICAL)
 
 
-def run_job_old(session, config, job, test_file):
-    print('\n\n\ntest_file:', test_file)
-    #print("run_job() job:", job['note'])
-    assert isinstance(job, dict)
-    assert isinstance(job['debug'], bool)
-    assert isinstance(job['echo'], bool)
-    if job['echo']:
-        pp.pprint(job)
-
-    jobs = job['inputs']
-    assert isinstance(jobs, list)
-    for test in jobs:
-        print("test:", test)
-        orm_object = test['orm_object']
-        assert ' ' not in orm_object
-        print("orm_object:", orm_object)
-        orm_object_attr = orm_object.lower()
-        orm_object = eval(orm_object)
-        print("orm_object:", orm_object)
-        orm_object_instance = orm_object.construct(session, tag=test[orm_object_attr], debug=job['debug'])
-        print("orm_object_instance:", orm_object_instance)
-        print("dir(orm_object_instance):", dir(orm_object_instance))
-        attrs_to_check_dict = test['str_attrs']
-        #print('attrs_to_check_dict:', attrs_to_check_dict)
-        for attr in attrs_to_check_dict.keys():
-            print("\nattr:", attr)
-            result = getattr(orm_object_instance, attr)
-            print("result:", result)
-            print("type(result):", type(result))
-            expected_result = attrs_to_check_dict[attr]
-            print("expected_result:", expected_result)
-            print("type(expected_result):", type(expected_result))
-            if expected_result: # it's not empty
-                try:
-                    assert result == expected_result
-                except AssertionError as e:
-                    print("\nAssertionError on attribute:", attr)
-                    print("result != expected_result:\n", result, "!=", expected_result)
-                    from IPython import embed; embed()
-                    raise e
-            else: #it's empty, so compare the empty sets?
-                try:
-                    assert len(result) == 0
-                except AssertionError as e:
-                    print("\nAssertionError on attribute:", attr)
-                    print("len(result) != 0:\n", len(result), "!= 0")
-                    #from IPython import embed; embed()
-                    raise e
-                try:
-                    assert set(result) == set(expected_result)
-                except AssertionError as e:
-                    print("\nAssertionError on attribute:", attr)
-                    print("set(result) != set(expected_result):\n", set(result), "!=", set(expected_result))
-                    #from IPython import embed; embed()
-                    raise e
-
-    session.commit()
-
 
 def check_db_result(db_result)
     ENGINE = get_engine(config)
