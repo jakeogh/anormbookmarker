02040090d04bdf07cb67b1b7ac8183e7f8f7f5b6
jakeogh <github.com@v6y.net>
2017-05-15 00:29:20 -0700

    auto-commit

--- anormbookmarker/Timestamp.py
+++ anormbookmarker/Timestamp.py
@@ -4,7 +4,6 @@
 
 import os
 import decimal
-#import datetime
 from sqlalchemy import Numeric
 from sqlalchemy import Column
 from sqlalchemy import Integer
@@ -12,14 +11,12 @@ from sqlalchemy import CheckConstraint
 from sqlalchemy.types import DateTime
 from .BaseMixin import BASE
 from sqlalchemy.sql import func
-
+from .get_one_or_create import get_one_or_create
 
 # might want a to add a Date type http://www.postgresql.org/docs/9.4/static/datatype-datetime.html
 class Timestamp(BASE):
-
     id = Column(Integer, primary_key=True)
     timestamp = Column(DateTime(timezone=True), unique=True, nullable=False, index=True, server_default=func.now())
-
 
     @classmethod
     def construct(cls, session):
