b2e6d8c55414eed36396ae08a29afad90842f008
jakeogh <github.com@v6y.net>
2017-05-14 20:53:04 -0700

    more tests, full coverage per orig_run_tests.py

--- anormbookmarker/test/tests/Alias/conflicting_with_existing_tag.py
+++ anormbookmarker/test/tests/Alias/conflicting_with_existing_tag.py
@@ -2,35 +2,37 @@
 
 from anormbookmarker.test.functions import *
 
-# make a tag to make an alias to
+# make tag
 eucalyptus_deglupta = Tag.construct(session=SESSION, tag='Eucalyptus deglupta')
 SESSION.commit()
 
-# make a tag to use in a conflicting alias for rainbow eucalyptus
+# make a tag to use in a conflicting alias for 'Eucalyptus deglupta'
 trees = Tag.construct(session=SESSION, tag='trees')
 SESSION.commit()
 
-# make a Alias
-alias = Alias.construct(session=SESSION, tag=eucalyptus_deglupta, alias='rainbow eucalyptus')
-SESSION.commit()
-
+# make conflicting Alias
 try:
-    # make a duplicate (conflicting) Alias to a different tag (correctly throws ConflictingAliasError)
-    alias = Alias.construct(session=SESSION, tag=trees, alias='rainbow eucalyptus')
+    alias = Alias.construct(session=SESSION, tag=trees, alias='Eucalyptus deglupta')
 except ConflictingAliasError:
     print("Correctly throws ConflictingAliasError")
-
 SESSION.commit()
 
-db_result = [('select COUNT(*) from alias;', 1),
-             ('select COUNT(*) from aliasword;', 2),
+#try:
+#    # make a duplicate (conflicting) Alias to a different tag (correctly throws ConflictingAliasError)
+#    alias = Alias.construct(session=SESSION, tag=trees, alias='rainbow eucalyptus')
+#except ConflictingAliasError:
+#    print("Correctly throws ConflictingAliasError")
+#SESSION.commit()
+
+db_result = [('select COUNT(*) from alias;', 0),
+             ('select COUNT(*) from aliasword;', 0),
              ('select COUNT(*) from bookmark;', 0),
              ('select COUNT(*) from filename;', 0),
              ('select COUNT(*) from tag;', 2),
              ('select COUNT(*) from tag_relationship;', 0),
              ('select COUNT(*) from tagbookmarks;', 0),
              ('select COUNT(*) from tagword;', 3),
-             ('select COUNT(*) from word;', 5),
+             ('select COUNT(*) from word;', 3),
              ('select COUNT(*) from wordmisspelling;', 0)]
 
 check_db_result(config=CONFIG, db_result=db_result)
---
+++ anormbookmarker/test/tests/Tag/parent_self.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from anormbookmarker.test.functions import *
+from sqlalchemy.exc import IntegrityError
+
+plants = Tag.construct(session=SESSION, tag='plants')
+SESSION.commit()
+
+plants.parents.append(plants)
+try:
+    SESSION.commit()
+except IntegrityError:
+    print("correctly throws IntegrityError")
+
+db_result = [('select COUNT(*) from alias;', 0),
+             ('select COUNT(*) from aliasword;', 0),
+             ('select COUNT(*) from bookmark;', 0),
+             ('select COUNT(*) from filename;', 0),
+             ('select COUNT(*) from tag;', 1),
+             ('select COUNT(*) from tag_relationship;', 0),
+             ('select COUNT(*) from tagbookmarks;', 0),
+             ('select COUNT(*) from tagword;', 1),
+             ('select COUNT(*) from word;', 1),
+             ('select COUNT(*) from wordmisspelling;', 0)]
+
+check_db_result(config=CONFIG, db_result=db_result)
+SESSION.close()
