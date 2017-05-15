8a0e645bce08b44c264339aa5c21376ad1745597
jakeogh <github.com@v6y.net>
2017-05-15 00:07:11 -0700

    auto-commit

--- anormbookmarker/Timestamp.py
+++ anormbookmarker/Timestamp.py
@@ -11,6 +11,8 @@ from sqlalchemy import Integer
 from sqlalchemy import CheckConstraint
 from sqlalchemy.types import DateTime
 from .BaseMixin import BASE
+from sqlalchemy.sql import func
+
 
 # might want a to add a Date type http://www.postgresql.org/docs/9.4/static/datatype-datetime.html
 class Timestamp(BASE):
@@ -20,7 +22,7 @@ class Timestamp(BASE):
     # timestamps should be unique
 
     id = Column(Integer, primary_key=True)
-    timestamp = Column(DateTime(timezone=True), unique=True, nullable=False, index=True, default=datetime.datetime.utcnow)
+    timestamp = Column(DateTime(timezone=True), unique=True, nullable=False, index=True, server_default=func.now())
 
     #def __init__(self, timestamp=None):  # todo disable passing of timestamp after oldschool import is done
     #    if timestamp:
