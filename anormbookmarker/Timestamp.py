9034d75e2f4cc9aa1946ba1da67daf851aa0bc49
jakeogh <github.com@v6y.net>
2017-05-14 22:10:45 -0700

    auto-commit

---
+++ anormbookmarker/Timestamp.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# MIT License
+
+import os
+import decimal
+from sqlalchemy import Numeric
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import CheckConstraint
+from sqlalchemy.types import DateTime
+
+# might want a to add a Date type http://www.postgresql.org/docs/9.4/static/datatype-datetime.html
+class Timestamp(Base):
+    # 1431237850.2934275 (os.stat().st_mtime)
+    # query returns a padded result
+    # 1396732434.7183542000000000000000
+    # timestamps should be unique
+
+    id = Column(Integer, primary_key=True)
+    #timestamp = Column(Numeric(32,22), CheckConstraint('timestamp>0'), unique=True, nullable=False, index=True)
+    timestamp = Column(DateTime(timezone=True), unique=True, nullable=False, index=True)
+
+    def __init__(self, timestamp=None):  # todo disable passing of timestamp after oldschool import is done
+        if timestamp:
+            self.timestamp = decimal.Decimal(format(decimal.Decimal(str(timestamp)), '0.22f'))
+
+    def __repr__(self):
+        return str(self.timestamp)
+
