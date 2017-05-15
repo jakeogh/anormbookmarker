16cd5346aca8502ecdfa7a7672e062482992e6c7
jakeogh <github.com@v6y.net>
2017-05-15 00:27:32 -0700

    auto-commit

--- anormbookmarker/Timestamp.py
+++ anormbookmarker/Timestamp.py
@@ -22,12 +22,10 @@ class Timestamp(BASE):
 
 
     @classmethod
-        def construct(cls, session):
-            result = get_one_or_create(session, Timestamp)
-            #print("returning result:", result)
-            return result
-
-
+    def construct(cls, session):
+        result = get_one_or_create(session, Timestamp)
+        #print("returning result:", result)
+        return result
 
     def __repr__(self):
         return str(self.timestamp)
