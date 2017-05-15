70cfd07656abfa2a73bb39a774278dc4d9d95546
jakeogh <github.com@v6y.net>
2017-05-14 19:58:29 -0700

    auto-commit

--- anormbookmarker/find_alias.py
+++ anormbookmarker/find_alias.py
@@ -7,9 +7,6 @@ from .Word import Word
 from .Word import WordMisSpelling
 from .AliasWord import AliasWord
 
-class ConflictingAliasError(ValueError):
-    pass
-
 def find_alias(session, alias, tag=False):
     '''
     iterates over the aliaswords table to check for a existing alias
@@ -18,29 +15,50 @@ def find_alias(session, alias, tag=False):
     it's reasonable to return the tag if it's found beacuse unlike an alias, a tag cant point to
     the wrong thing. returning a "duplicate" alias only makes sense if it's pointing to the same tag.
     '''
-    #print("find_alias() alias:", alias)
     corrected_alias = alias
     possible_alias_set = set([])
     assert isinstance(alias, str)
+
+    alias_split = alias.split(' ')
+
+
+    corrected_alias = alias
+    for index, word in enumerate(alias_split):
+
+        try:
+            wordmisspelling = session.query(WordMisSpelling).filter_by(wordmisspelling=word).one()
+            target_word = wordmisspelling.word
+            word = str(target_word)
+            corrected_alias = alias.replace(wordmisspelling.wordmisspelling, word)
+        except NoResultFound:
+
+            pass
+
+
+    corrected_alias_split = corrected_alias.split(' ')
+
     try:
-        for index, word in enumerate(alias.split(' ')):
-            try:
-                wordmisspelling = session.query(WordMisSpelling).filter_by(wordmisspelling=word).one()
-                target_word = wordmisspelling.word
-                word = str(target_word)
-                corrected_alias = alias.replace(wordmisspelling.wordmisspelling, word)
-            except NoResultFound:
-                pass
+        for index, word in enumerate(corrected_alias_split):
+
             current_word = session.query(Word).filter_by(word=word).one()
+
             current_aliasword_list = session.query(AliasWord).filter_by(word=current_word, position=index).all()
+
             if current_aliasword_list:
+
                 current_aliasword_list_alias_set = set([aliasword.alias for aliasword in current_aliasword_list])
-                possible_alias_set = possible_alias_set & current_aliasword_list_alias_set
+
+                if not possible_alias_set:
+                    possible_alias_set = current_aliasword_list_alias_set
+                else:
+                    possible_alias_set = possible_alias_set & current_aliasword_list_alias_set
+
                 for aliasword in current_aliasword_list:
                     if index == 0: # only add aliass that start with the correct word
                         possible_alias_set.add(aliasword.alias)
                     else:
                         if aliasword.alias not in possible_alias_set:
+
                             return False
                 if not possible_alias_set:
                     return False
@@ -60,3 +78,6 @@ def find_alias(session, alias, tag=False):
                         return False
     except NoResultFound: # any failed query
         return False
+
+class ConflictingAliasError(ValueError):
+    pass
