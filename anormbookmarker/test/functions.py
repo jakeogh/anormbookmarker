7d2cb1a5536eb63cdca0aa941c372ea5290bd2de
jakeogh <github.com@v6y.net>
2017-05-19 15:03:19 -0700

    auto-commit

--- anormbookmarker/test/functions.py
+++ anormbookmarker/test/functions.py
@@ -34,12 +34,13 @@ def check_db_result(config, db_result):
     ENGINE = get_engine(config)
     tables = set(ENGINE.table_names())
     print("tables:", tables)
+    assert tables
 
     for db_test in db_result:
         print(db_test)
         db_test_table = db_test[0].split()[-1].split(';')[0]
         #print("db_test_table:", db_test_table)
-        tables = tables.remove(db_test_table)
+        tables.remove(db_test_table)
         with ENGINE.connect() as connection:
             answer = connection.execute(db_test[0])
             for row in answer:
@@ -49,8 +50,8 @@ def check_db_result(config, db_result):
                     print("\nAssertionError on db test:", db_test[0])
                     print("row[0] != db_test[0]:\n", row[0], "!=", db_test[1])
                     raise e
-        if tables:
-            print("Missed table test(s):", tables)
-        assert not tables
+    if tables:
+        print("Missed table test(s):", tables)
+    assert not tables
 
     ENGINE.dispose()
