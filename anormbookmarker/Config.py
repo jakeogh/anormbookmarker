4b1f88f952d2829ace478784cb5571a652bfe592
jakeogh <github.com@v6y.net>
2017-11-25 23:33:57 -0700

    auto-commit

--- anormbookmarker/Config.py
+++ anormbookmarker/Config.py
@@ -11,8 +11,8 @@ class Config():
     def __init__(self):
         self.word_max_length = 255
         self.tag_max_length = 255
-        self.dbname = 'anormbookmarker_test_' + str(time.time()).replace('.', '_')
-        self.database = 'postgresql://postgres@localhost/' + self.dbname
+        self.timestamp_dbname = 'anormbookmarker_test_' + str(time.time()).replace('.', '_')
+        self.timestamp_database = 'postgresql://postgres@localhost/' + self.timestamp_dbname
         self.pg_dbpath = 'postgresql://postgres@localhost/postgres'
 
 CONFIG = Config()
