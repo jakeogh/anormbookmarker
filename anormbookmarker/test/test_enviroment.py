7eba0bc9864c04ec53cc091f262d681d76f5d1b2
jakeogh <github.com@v6y.net>
2017-11-30 22:04:50 -0700

    auto-commit

--- anormbookmarker/test/test_enviroment.py
+++ anormbookmarker/test/test_enviroment.py
@@ -3,7 +3,6 @@
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-
 #from anormbookmarker.test.Filename import Filename
 from kcl.sqlalchemy.Filename import Filename
 from anormbookmarker.TagClassConstructor import TagClassConstructor
@@ -16,10 +15,10 @@ from anormbookmarker.Alias import Alias
 from anormbookmarker.Word import WordMisSpelling
 from anormbookmarker.Word import Word
 from anormbookmarker.Config import CONFIG
-from kcl.postgresqlops import get_engine
-from kcl.postgresqlops import create_database_and_tables
-from kcl.postgresqlops import create_session
-from kcl.postgresqlops import delete_database
+from kcl.sqlalchemy.create_database_and_tables import create_database_and_tables
+from kcl.sqlalchemy.create_session import create_session
+from kcl.sqlalchemy.delete_database import delete_database
+from kcl.sqlalchemy.check_db_result import check_db_result
 from anormbookmarker.find_alias import ConflictingAliasError
 from kcl.sqlalchemy.BaseMixin import BASE
 
@@ -32,4 +31,3 @@ logging.getLogger('sqlalchemy.engine').setLevel(logging.CRITICAL)
 create_database_and_tables(database=CONFIG.timestamp_database, schema=BASE)
 SESSION = create_session(database=CONFIG.timestamp_database, multithread=False)
 
-from kcl.sqlalchemy.check_db_result import check_db_result
