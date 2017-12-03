05cd78663debd352a37528cdaab73e5f8116f090
jakeogh <github.com@v6y.net>
2017-12-03 16:34:04 -0700

    auto-commit

--- anormbookmarker/test/tests/Word/end_letters_swapped.py
+++ anormbookmarker/test/tests/Word/end_letters_swapped.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 from anormbookmarker.test.test_enviroment import *
+with self_contained_session(CONFIG.timestamp_database) as session:
+    BASE.metadata.create_all(session.bind)
 
-buffalo = Word.construct(session=SESSION, word='Buffalo')
-SESSION.commit()
+    buffalo = Word.construct(session=SESSION, word='Buffalo')
+    SESSION.commit()
 
-buffalo_swap = Word.construct(session=SESSION, word='ouffalB')
-SESSION.commit()
+    buffalo_swap = Word.construct(session=SESSION, word='ouffalB')
+    SESSION.commit()
 
 db_result = [('select COUNT(*) from alias;', 0),
              ('select COUNT(*) from aliasword;', 0),
