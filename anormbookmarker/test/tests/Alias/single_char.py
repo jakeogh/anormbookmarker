3c37f7202addb7619fe5d8277d5addde9e47898b
jakeogh <github.com@v6y.net>
2017-12-03 20:01:16 -0700

    auto-commit

---
+++ anormbookmarker/test/tests/Alias/single_char.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from anormbookmarker.test.test_enviroment import *
+with self_contained_session(CONFIG.timestamp_database) as session:
+    BASE.metadata.create_all(session.bind)
+
+    # make a tag to make an alias to
+    e_uppercase_tag = Tag.construct(session=session, tag='E')
+    session.commit()
+
+    # make a Alias # todo, this single char case shouldnt be allowed, prob no single AliasWord Alias instances
+    #alias = Alias.construct(session=session, tag=eucalyptus_deglupta, alias='rainbow eucalyptus', casesensitive=False)
+    e_lowercase_alias = Alias.construct(session=session, tag=euppercase_tag, alias='e')
+    session.commit()
+
+db_result = [('select COUNT(*) from alias;', 1),
+             ('select COUNT(*) from aliasword;', 2),
+             ('select COUNT(*) from bookmark;', 0),
+             ('select COUNT(*) from filename;', 0),
+             ('select COUNT(*) from tag;', 1),
+             ('select COUNT(*) from tag_relationship;', 0),
+             ('select COUNT(*) from tagbookmarks;', 0),
+             ('select COUNT(*) from tagword;', 2),
+             ('select COUNT(*) from word;', 4),
+             ('select COUNT(*) from wordmisspelling;', 0)]
+
+check_db_result(config=CONFIG, db_result=db_result)
