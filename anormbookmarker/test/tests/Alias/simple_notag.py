b6a7ab747a106736be99169c499b8e3057d5f8b9
jakeogh <github.com@v6y.net>
2017-12-15 00:49:30 -0700

    auto-commit

--- anormbookmarker/test/tests/Alias/simple_notag.py
+++ anormbookmarker/test/tests/Alias/simple_notag.py
@@ -20,10 +20,10 @@ db_result = [('select COUNT(*) from alias;', 1),
              ('select COUNT(*) from aliasword;', 2),
              ('select COUNT(*) from bookmark;', 0),
              ('select COUNT(*) from filename;', 0),
-             ('select COUNT(*) from tag;', 0),
+             ('select COUNT(*) from tag;', 1),
              ('select COUNT(*) from tag_relationship;', 0),
              ('select COUNT(*) from tagbookmarks;', 0),
-             ('select COUNT(*) from tagword;', 0),
+             ('select COUNT(*) from tagword;', 2),
              ('select COUNT(*) from word;', 2),
              ('select COUNT(*) from wordmisspelling;', 0)]
 
