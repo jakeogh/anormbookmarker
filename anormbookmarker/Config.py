f2b8c0751213b388b402bc465c477948f74041f1
jakeogh <github.com@v6y.net>
2017-12-23 21:28:00 -0700

    auto-commit

--- anormbookmarker/Config.py
+++ anormbookmarker/Config.py
@@ -9,6 +9,7 @@ from kcl.sqlalchemy.model.BaseConfig import BaseConfig
 class Config(BaseConfig):
     '''Simple configuration class.'''
     def __init__(self):
+        BaseConfig.__init__(self)
         self.word_max_length = 255
         self.tag_max_length = 255
         #self.timestamp_dbname = 'anormbookmarker_test_' + str(time.time()).replace('.', '_')
