9812fecdaf3e23b73c794ba0c33fc3bee3598783
jakeogh <github.com@v6y.net>
2017-12-23 21:51:45 -0700

    auto-commit

--- anormbookmarker/test/test_enviroment.py
+++ anormbookmarker/test/test_enviroment.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-
 from kcl.sqlalchemy.model.Filename import Filename
 from anormbookmarker.TagClassConstructor import TagClassConstructor
 Tag = TagClassConstructor(mapper_to_bookmark=Filename)
@@ -14,20 +11,5 @@ from anormbookmarker.Alias import Alias
 from anormbookmarker.Word import WordMisSpelling
 from anormbookmarker.Word import Word
 from anormbookmarker.Config import CONFIG
-from sqlalchemy_utils.functions import create_database
-from kcl.sqlalchemy.delete_database import delete_database
-from kcl.sqlalchemy.check_db_result import check_db_result
 from anormbookmarker.find_alias import ConflictingAliasError
-from kcl.sqlalchemy.BaseMixin import BASE
-from kcl.sqlalchemy.self_contained_session import self_contained_session
-from kcl.printops import eprint
-
-import logging
-logger = logging.getLogger()
-logger.setLevel(logging.CRITICAL)
-logging.basicConfig()
-logging.getLogger('sqlalchemy.engine').setLevel(logging.CRITICAL)
-
-CONFIG.database = CONFIG.database_timestamp
-
-create_database(CONFIG.database)
+from kcl.sqlalchemy.base_test_enviroment import *
