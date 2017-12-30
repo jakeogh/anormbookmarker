ead260c1449a459fca87291c666bc278af7fc725
jakeogh <github.com@v6y.net>
2017-12-29 23:01:23 -0700

    auto-commit

--- anormbookmarker/anormbookmarker.py
+++ anormbookmarker/anormbookmarker.py
@@ -6,18 +6,14 @@ from anormbookmarker.model.Config import CONFIG
 builtins.config = CONFIG
 
 from kcl.sqlalchemy.clickapp.clickapp import clickapp as anormbookmarker
+builtins.appobject = anormbookmarker
 anormbookmarker.help = CONFIG.appname + " interface"
 
 from kcl.sqlalchemy.clickapp.default import *
-anormbookmarker.add_command(ipython)
-anormbookmarker.add_command(print_database)
-anormbookmarker.add_command(test)
-anormbookmarker.add_command(show_config, name="config")
 
 from .cli.visualization.sa_display import sa_display
 #from .cli.list_objects.list_objects import list_objects
 #from .cli.create_objects.create_objects import create_objects
 
 anormbookmarker.add_command(sa_display)
-
 #anormbookmarker.add_command(display_database)
--- anormbookmarker/model/ByteString.py
+++ anormbookmarker/model/ByteString.py
@@ -4,27 +4,26 @@
 '''
     Generic 'group of bytes' type to collect all strings (bytes, unicode whatever) into a easy to search index
 '''
-
-from kcl.hashops import bytes_dict_bytes
-from iridb.model.Config import config
-from sqlalchemy import Column           #                                                                                          http://docs.sqlalchemy.org/en/rel_0_9/core/metadata.html#sqlalchemy.schema.Column
-from sqlalchemy import ForeignKey       #                                                                                          http://docs.sqlalchemy.org/en/rel_0_9/core/metadata.html#sqlalchemy.schema.Column
-from sqlalchemy import Integer          # int4 integer 4 bytes -2147483648 to +2147483647 (typical timestamp: 1,400,384,814)       http://docs.sqlalchemy.org/en/rel_0_9/core/type_basics.html#sqlalchemy.types.uInteger
-from sqlalchemy import Unicode          # Unicode type is a String subclass that assumes input and output as Python unicode data   http://docs.sqlalchemy.org/en/rel_0_9/core/type_basics.html#sqlalchemy.types.Unicode
+from anormbookmarker.model.Config import CONFIG
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import Unicode
 from sqlalchemy.orm import relationship
 from sqlalchemy.schema import Sequence
+from kcl.hashops import bytes_dict_bytes
 from kcl.sqlalchemy.get_one_or_create import get_one_or_create
 from kcl.sqlalchemy.BaseMixin import BASE
-from iridb.model.Hash import Hash
+from kcl.sqlalchemy.model.BytesHash import BytesHash
 
 # some iri's (and uri's) are too long to have a unique constraint so using unique=True on hash instead
 class ByteString(BASE):
     # store all iri's, uri's together - 99% of iri's will match the uri
     id = Column(Integer, Sequence('id_seq', start=0, minvalue=0), autoincrement=True, primary_key=True, index=True)
     # IRI_MAX_LENGTH is too long to unique or index
-    byte_string = Column(Unicode(config.iri_max_length), unique=False, nullable=False, index=False)
+    byte_string = Column(Unicode(CONFIG.iri_max_length), unique=False, nullable=False, index=False)
     hash_id = Column(Integer, ForeignKey('hash.id'), unique=True, nullable=False, index=True)
-    hash = relationship('Hash', backref='byte_string')
+    hash = relationship('BytesHash', backref='byte_string')
 
     @classmethod
     def construct(cls, session, byte_string):
@@ -34,7 +33,7 @@ class ByteString(BASE):
         # todo: add test for bytes tag
         # todo: feed the tag system with angryfiles
         hash_dict = bytes_dict_bytes(byte_string)
-        hash = get_one_or_create(session, Hash, **hash_dict)
+        hash = get_one_or_create(session, BytesHash, **hash_dict)
         result = get_one_or_create(session, ByteString, byte_string=byte_string, hash=hash)
         assert isinstance(result, ByteString)
         return result
