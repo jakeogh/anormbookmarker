189ca07885419a3bc55cd990919e181d50adb092
jakeogh <github.com@v6y.net>
2017-12-29 21:48:42 -0700

    auto-commit

--- anormbookmarker/anormbookmarker.py
+++ anormbookmarker/anormbookmarker.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 
-
 import click
 import builtins
 from anormbookmarker.model.Config import CONFIG
 builtins.config = CONFIG
 
 from kcl.sqlalchemy.clickapp.clickapp import clickapp as anormbookmarker
-from kcl.sqlalchemy.clickapp.print_database import print_database
-from kcl.sqlalchemy.clickapp.test import test
-from kcl.sqlalchemy.clickapp.show_config import show_config
-from kcl.sqlalchemy.ipython import ipython
+anormbookmarker.help = CONFIG.appname + " interface"
+
+from kcl.sqlalchemy.clickapp.default import *
+anormbookmarker.add_command(ipython)
+anormbookmarker.add_command(print_database)
+anormbookmarker.add_command(test)
+anormbookmarker.add_command(show_config, name="config")
+
 from .cli.visualization.sa_display import sa_display
 #from .cli.list_objects.list_objects import list_objects
 #from .cli.create_objects.create_objects import create_objects
 
-anormbookmarker.help = CONFIG.appname + " interface"
+fsindex.add_command(sa_display)
 
-anormbookmarker.add_command(ipython)
-anormbookmarker.add_command(print_database)
-
-anormbookmarker.add_command(show_config, name='config')
 #anormbookmarker.add_command(display_database)
-anormbookmarker.add_command(test)
