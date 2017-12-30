856d0760eafd0d9215b6a889ebf390d992f246db
jakeogh <github.com@v6y.net>
2017-12-29 21:33:35 -0700

    auto-commit

--- anormbookmarker/anormbookmarker.py
+++ anormbookmarker/anormbookmarker.py
@@ -9,6 +9,7 @@ builtins.config = CONFIG
 from kcl.sqlalchemy.clickapp.clickapp import clickapp as anormbookmarker
 from kcl.sqlalchemy.clickapp.print_database import print_database
 from kcl.sqlalchemy.clickapp.test import test
+from kcl.sqlalchemy.clickapp.show_config import show_config
 from kcl.sqlalchemy.ipython import ipython
 from .cli.visualization.sa_display import sa_display
 #from .cli.list_objects.list_objects import list_objects
@@ -19,6 +20,6 @@ anormbookmarker.help = CONFIG.appname + " interface"
 anormbookmarker.add_command(ipython)
 anormbookmarker.add_command(print_database)
 
-#anormbookmarker.add_command(show_config, name='config')
+anormbookmarker.add_command(show_config, name='config')
 #anormbookmarker.add_command(display_database)
 anormbookmarker.add_command(test)
