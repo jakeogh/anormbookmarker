63cdfa9ff8a96cb2fc942f5984a481e5613baea5
jakeogh <github.com@v6y.net>
2017-12-31 07:43:41 -0700

    auto-commit

--- anormbookmarker/cli/visualization/sa_display.py
+++ anormbookmarker/cli/visualization/sa_display.py
@@ -4,6 +4,7 @@ import click
 from anormbookmarker.model import *
 from anormbookmarker.test.test_enviroment import Tag
 from anormbookmarker.test.test_enviroment import Bookmark
+from kcl.sqlalchemy.model.Filename import Filename
 #from kcl.sqlalchemy.model.FileRecord import Filename
 #from kcl.sqlalchemy.model.FileRecord import Path
 from kcl.sqlalchemy.visualization.sa_display import sa_display as kcl_sa_display
