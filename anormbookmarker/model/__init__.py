dad7baaaffabe61ed55c971ff582d47ad8060f02
jakeogh <github.com@v6y.net>
2017-12-31 07:21:52 -0700

    auto-commit

---
+++ anormbookmarker/model/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+## https://stackoverflow.com/questions/1057431/how-to-load-all-modules-in-a-folder
+
+import os
+module_path = os.path.dirname(__file__)
+for module in os.listdir(module_path):
+    if module == '__init__.py' or module[-3:] != '.py' or not module[0].isupper():
+        continue
+    classname = module[:-3]
+    __import__('iridb.model.'+classname, globals=globals(), locals=locals(), fromlist=[classname], level=0)
+    cmd_to_eval = classname + ' = ' + classname + '.' + classname
+    exec(cmd_to_eval) #sshhhhhhhh
+del module
