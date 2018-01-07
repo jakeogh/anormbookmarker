41dd4a4b3b6ca5b2a1b50caa454209966c694e8f
jakeogh <github.com@v6y.net>
2018-01-06 20:00:26 -0700

    auto-commit

--- anormbookmarker/model/Bookmark.py
+++ anormbookmarker/model/Bookmark.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 from kcl.sqlalchemy.model.Filename import Filename
 from anormbookmarker.model.BookmarkClassConstructor import BookmarkClassConstructor
 Bookmark = BookmarkClassConstructor(mapper_to_bookmark=Filename)
